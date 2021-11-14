e65bbdd5558637a0d70d2ea2a7c2bcb94d5b451d
zhaoxin0311 <1204090769@qq.com>
2021-11-14 19:20:59 +0800

    tijiso

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
