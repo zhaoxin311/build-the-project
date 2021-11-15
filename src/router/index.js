fb8e183919b7f6a53c3408b6abc233560382390a
zhaoxin0311 <1204090769@qq.com>
2021-11-15 16:37:32 +0800

    添加路由2

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,43 @@ const routes = [
     component: () => import("../views/About.vue"),
   },
   {
+    path: "/icon",
+    component: Layout,
+    children: [
+      // {
+      //   path: "index",
+      //   component: () => import("svg-icons/index"),
+      //   name: "icons",
+      //   meta: { title: "icons", icon: "icon" },
+      // },
+    ],
+  },
+
+  // Has submenu, because children.length>=1
+  {
+    path: "/components",
+    component: Layout,
+    name: "component-demo",
+    meta: {
+      title: "components",
+      icon: "component",
+    },
+    children: [
+      // {
+      //   path: "tinymce",
+      //   component: () => import("components-demo/tinymce"),
+      //   name: "tinymce-demo",
+      //   meta: { title: "tinymce" },
+      // },
+      // {
+      //   path: "markdown",
+      //   component: () => import("components-demo/markdown"),
+      //   name: "markdown-demo",
+      //   meta: { title: "markdown" },
+      // },
+    ],
+  },
+  {
     path: "/permission",
     component: Layout,
     //当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
@@ -37,17 +74,17 @@ const routes = [
     alwaysShow: true, //一直显示根路由
     meta: { roles: ["admin", "editor"] }, //你可以在根路由设置权限，这样它下面所有的子路由都继承了这个权限
     children: [
-      {
-        path: "index",
-        component: () => import("permission/index"),
-        name: "permission", // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
-        meta: {
-          title: "permission", // 设置该路由在侧边栏和面包屑中展示的名字
-          icon: "lock", //路由的图标，支持 svg-class，也支持 el-icon-x element-ui 的 icon
-          roles: ["admin", "editor"], // 设置该路由进入的权限，支持多个权限叠加//或者你可以给每一个子路由设置自己的权限
-          noCache: true, // 为true，不会被 <keep-alive> 缓存
-        },
-      },
+      // {
+      //   path: "index",
+      //   component: () => import("permission/index"),
+      //   name: "permission", // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
+      //   meta: {
+      //     title: "permission", // 设置该路由在侧边栏和面包屑中展示的名字
+      //     icon: "lock", //路由的图标，支持 svg-class，也支持 el-icon-x element-ui 的 icon
+      //     roles: ["admin", "editor"], // 设置该路由进入的权限，支持多个权限叠加//或者你可以给每一个子路由设置自己的权限
+      //     noCache: true, // 为true，不会被 <keep-alive> 缓存
+      //   },
+      // },
     ],
   },
   // 404 page must be placed at the end !!!
