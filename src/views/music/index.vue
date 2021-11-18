adb2d7aa5b01bf2aada868cedbf8714a2eae5a6c
zhaoxin0311 <1204090769@qq.com>
2021-11-18 17:01:14 +0800

    tijiao

--- src/views/music/index.vue
+++ src/views/music/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div><h1>music page</h1></div>
+  <div>
+    <h1>music page</h1>
+    <h1>music page</h1>
+    <h1>music page</h1>
+    <h1>music page</h1>
+    <h1>music page</h1>
+  </div>
 </template>
