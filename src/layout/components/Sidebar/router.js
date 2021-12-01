93e6c3bbdcbde4195c623c6609e62e43caa0dc89
zhaoxin0311 <1204090769@qq.com>
2021-12-01 16:19:48 +0800

    tijiao

--- src/layout/components/Sidebar/router.js
+++ src/layout/components/Sidebar/router.js
@@ -1,27 +1,27 @@
-export default [
-  {
-    path: "/home",
-    name: "系统首页",
-    icon: "el-icon-s-home",
-  },
-  {
-    path: "/xinwenzixun",
-    name: "新闻资讯",
-    icon: "el-icon-s-management",
-  },
-  {
-    path: "/shoucangguanli",
-    name: "收藏管理",
-    icon: "el-icon-star-on",
-  },
-  {
-    path: "/liebiaoguanli",
-    name: "列表管理",
-    icon: "el-icon-s-operation",
-  },
-  {
-    path: "/gedanguanli",
-    name: "歌单管理",
-    icon: "el-icon-message-solid",
-  },
-];
+// export default [
+//   {
+//     path: "/home",
+//     name: "系统首页",
+//     icon: "el-icon-s-home",
+//   },
+//   {
+//     path: "/xinwenzixun",
+//     name: "新闻资讯",
+//     icon: "el-icon-s-management",
+//   },
+//   {
+//     path: "/shoucangguanli",
+//     name: "收藏管理",
+//     icon: "el-icon-star-on",
+//   },
+//   {
+//     path: "/liebiaoguanli",
+//     name: "列表管理",
+//     icon: "el-icon-s-operation",
+//   },
+//   {
+//     path: "/gedanguanli",
+//     name: "歌单管理",
+//     icon: "el-icon-message-solid",
+//   },
+// ];
